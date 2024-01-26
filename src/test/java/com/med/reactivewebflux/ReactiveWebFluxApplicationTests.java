9d8b63eb554d8a254c676492d60f4f6714bc7275
Zaghdoudi mohamed <mohamed.zaghdoudi@codeonce.fr>
2024-01-26 10:10:06 +0100

    first commit

---
+++ src/test/java/com/med/reactivewebflux/ReactiveWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.med.reactivewebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveWebFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
